451ba6f58a6a25edebdf2d6251c1145bc075b42a
carternadain <cnadain@gmail.com>
2023-01-25 16:03:19 -0700

    add nav bar code w/ bootstrap

--- client/src/components/Navbar/nav.js
+++ client/src/components/Navbar/nav.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 function Navigation() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="nav-item nav-link" to="/">Home</NavLink>
-      <NavLink className="nav-item nav-link" to="/about">About</NavLink>
-      <NavLink className="nav-item nav-link" to="/contact">Contact</NavLink>
+      <img src={logo} className="navbar-brand" alt="Logo" />
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sign/Sign Up
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Sign In</a>
+              <a className="dropdown-item" href="#">Sign Up</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/destinations">Destinations</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/trips">Trips</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/tips">Tips</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
